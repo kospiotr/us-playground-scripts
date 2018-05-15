8dfc1adebb3d3fe865adaf606f11ef2910627e12
Piotr Kosmowski <kospiotr@tlen.pl>
2018-05-15 11:27:47 +0200

    Update startup-server.sh

--- startup-server.sh
+++ startup-server.sh
@@ -4,10 +4,11 @@ export BRANCH=$1
 
 echo "Current branch is: ${BRANCH}"
 
-#bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}task-consul-install.sh")
-#bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}task-nomad-install.sh")
-#bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}task-linkerd-install.sh")
+echo "Installing CONSUL"
+bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}/task-consul-install.sh?nocache") > /var/log/task-consul-install.log
+bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}/task-nomad-install.sh?nocache") > /var/log/task-nomad-install.log
+bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}/task-linkerd-install.sh?nocache") > /var/log/task-linkerd-install.log
 
-#bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}task-consul-run-server.sh")
-#bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}task-nomad-run.sh")
-#bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}task-linkerd-run.sh")
+bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}/task-consul-run-server.sh?nocache") > /var/log/task-consul-run.log
+bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}/task-nomad-run.sh?nocache") > /var/log/task-nomad-run.log
+bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}/task-linkerd-run.sh?nocache") > /var/log/task-linkerd-run.log
