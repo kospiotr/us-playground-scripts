27cba194a4191b38cd2520d518eccba0d308a33f
solk <szymon.lyszkowski@gft.com>
2018-05-15 15:01:29 +0200

    Added linkerd version in run script.

--- task-linkerd-run.sh
+++ task-linkerd-run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo "Run Linkerd"
+LINKERD_VERSION=1.4.0
 nohup /tmp/linkerd-${LINKERD_VERSION}/linkerd-${LINKERD_VERSION}-exec /tmp/linkerd-config.yaml  2>&1 > /var/log/consul.log &
 echo "Linkerd is running"
