49705c23b6c96a28415db8755670669efc0a9960
Piotr Kosmowski <kospiotr@tlen.pl>
2018-05-15 11:11:53 +0200

    Update startup-bootstrap.sh

--- startup-bootstrap.sh
+++ startup-bootstrap.sh
@@ -4,7 +4,7 @@ export BRANCH=$1
 
 echo "Current branch is: ${BRANCH}"
 
-#bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}task-consul-install.sh")
+bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}task-consul-install.sh")
 #bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}task-nomad-install.sh")
 #bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}task-linkerd-install.sh")
 
