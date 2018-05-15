21ded8a56a1651bdf8731f492a4fd319fa3ad9dd
Piotr Kosmowski <kospiotr@tlen.pl>
2018-05-15 10:58:00 +0200

    Update task-consul-run-bootstrap.sh

--- task-consul-run-bootstrap.sh
+++ task-consul-run-bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Running Consul"
+echo "Running Consul in bootstrap and server mode"
 nohup consul agent -server \
     -bootstrap-expect=2 -ui -client 0.0.0.0 \
     -data-dir=/tmp/consul \
@@ -15,3 +15,5 @@ nohup consul agent -server \
     -retry-join=10.156.0.8 \
     -retry-join=10.156.0.9 \
     -retry-join=10.156.0.10 2>&1 > /var/log/consul.log &
+
+echo "Consul is running"
