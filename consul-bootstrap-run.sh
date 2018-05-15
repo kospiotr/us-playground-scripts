d8b35a974cee2053e789d458a568fc5e250e6362
Piotr Kosmowski <kospiotr@tlen.pl>
2018-05-15 10:42:06 +0200

    Create consul-bootstrap-run.sh

---
+++ consul-bootstrap-run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Running Consul"
+nohup consul agent -server \
+    -bootstrap-expect=2 -ui -client 0.0.0.0 \
+    -data-dir=/tmp/consul \
+    -enable-script-checks=true -config-dir=/etc/consul.d \
+    -retry-join=pk-api-bootstrap \
+    -retry-join=10.156.0.2 \
+    -retry-join=10.156.0.3 \
+    -retry-join=10.156.0.4 \
+    -retry-join=10.156.0.5 \
+    -retry-join=10.156.0.6 \
+    -retry-join=10.156.0.7 \
+    -retry-join=10.156.0.8 \
+    -retry-join=10.156.0.9 \
+    -retry-join=10.156.0.10 2>&1 > /var/log/consul.log &
