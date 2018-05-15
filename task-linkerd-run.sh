a725e659aee9ebe9036711e76b6039694b8587e5
solk <szymon.lyszkowski@gft.com>
2018-05-15 14:56:16 +0200

    Fix for running linkerd command.

--- task-linkerd-run.sh
+++ task-linkerd-run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Run Linkerd"
-nohup ./tmp/linkerd-${LINKERD_VERSION}/linkerd-${LINKERD_VERSION}-exec /tmp/linkerd-config.yaml  2>&1 > /var/log/consul.log &
+nohup /tmp/linkerd-${LINKERD_VERSION}/linkerd-${LINKERD_VERSION}-exec /tmp/linkerd-config.yaml  2>&1 > /var/log/consul.log &
 echo "Linkerd is running"
