49641869a8b05170c25362ac10055cb2d439304d
Piotr Kosmowski <kospiotr@tlen.pl>
2018-05-16 12:17:50 +0200

    Update startup-orchestrator.sh

--- startup-orchestrator.sh
+++ startup-orchestrator.sh
@@ -4,11 +4,8 @@ export BRANCH=$1
 
 echo "Current branch is: ${BRANCH}"
 
-echo "Installing CONSUL"
 bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}/task-consul-install.sh?nocache") > /var/log/task-consul-install.log
 bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}/task-nomad-install.sh?nocache") > /var/log/task-nomad-install.log
-bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}/task-linkerd-install.sh?nocache") > /var/log/task-linkerd-install.log
 
-bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}/task-consul-run-bootstrap.sh?nocache") > /var/log/task-consul-run.log
-bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}/task-nomad-run.sh?nocache") > /var/log/task-nomad-run.log
-bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}/task-linkerd-run.sh?nocache") > /var/log/task-linkerd-run.log
+bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}/task-consul-run-server.sh?nocache") > /var/log/task-consul-run.log
+bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}/task-nomad-server.sh?nocache") > /var/log/task-nomad-run.log
