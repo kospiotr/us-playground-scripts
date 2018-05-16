85ca6c76dba454b435d6344b739c261ed5e17a18
Piotr Kosmowski <kospiotr@tlen.pl>
2018-05-16 13:24:39 +0200

    Update and rename task-consul-run-server.sh to task-consul-run-orchestrate.sh

---
+++ task-consul-run-orchestrate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Running Consul in bootstrap and server mode"
+nohup consul agent -server \
+    -bootstrap-expect=2 -ui -client 0.0.0.0 \
+    -data-dir=/tmp/consul \
+    -enable-script-checks=true -config-dir=/etc/consul.d \
+    -retry-join=pk-api-orchestrate-1 2>&1 > /var/log/consul.log &
+
+echo "Consul is running"
