85c6d769dbc164917fcf3caf3bba0e6e2b3619bf
Piotr Kosmowski <kospiotr@tlen.pl>
2018-05-16 13:28:53 +0200

    Update task-consul-run-server.sh

--- task-consul-run-server.sh
+++ task-consul-run-server.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
 echo "Running Consul in server mode"
-nohup consul agent -server \
+nohup consul agent \
     -data-dir=/tmp/consul \
     -enable-script-checks=true -config-dir=/etc/consul.d \
-    -retry-join=pk-api-bootstrap \
-    -retry-join=10.156.0.2 \
-    -retry-join=10.156.0.3 \
-    -retry-join=10.156.0.4 \
-    -retry-join=10.156.0.5 \
-    -retry-join=10.156.0.6 \
-    -retry-join=10.156.0.7 \
-    -retry-join=10.156.0.8 \
-    -retry-join=10.156.0.9 \
-    -retry-join=10.156.0.10 2>&1 > /var/log/consul.log &
+    -retry-join=pk-api-orchestrator-1 2>&1 > /var/log/consul.log &
 
 echo "Consul is running"
