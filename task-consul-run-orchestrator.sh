954395cf0bc6cec0a604c462b08da08a29181c8e
Piotr Kosmowski <kospiotr@tlen.pl>
2018-05-16 13:26:06 +0200

    Update task-consul-run-orchestrator.sh

--- task-consul-run-orchestrator.sh
+++ task-consul-run-orchestrator.sh
@@ -2,7 +2,7 @@
 
 echo "Running Consul in bootstrap and server mode"
 nohup consul agent -server \
-    -bootstrap-expect=2 -ui -client 0.0.0.0 \
+    -bootstrap-expect=1 -ui -client 0.0.0.0 \
     -data-dir=/tmp/consul \
     -enable-script-checks=true -config-dir=/etc/consul.d \
     -retry-join=pk-api-orchestrator-1 2>&1 > /var/log/consul.log &
