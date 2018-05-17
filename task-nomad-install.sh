3441f305dd22321e080a1a4459d2ca297d668552
Piotr Kosmowski <kospiotr@tlen.pl>
2018-05-17 08:32:16 +0200

    Update task-nomad-install.sh

--- task-nomad-install.sh
+++ task-nomad-install.sh
@@ -21,15 +21,11 @@ sudo chmod a+w /etc/nomad.d
 sudo mkdir -p /var/nomad
 sudo chmod a+w /var/nomad
 
-NOMAD_SERVERS_NUMBER=2
-NOMAD_DC_NAME=dc1
-NOMAD_SERVER=true
-
 cat >/etc/nomad.d/server.hcl <<EOL
 data_dir = "/var/nomad"
 server {
   enabled          = true
-  bootstrap_expect = ${NOMAD_SERVERS_NUMBER}
+  bootstrap_expect = 2
 }
 client {
   enabled = true
