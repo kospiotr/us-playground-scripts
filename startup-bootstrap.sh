6170c9bea3757b00b1775adb4695c72ec298a712
Piotr Kosmowski <kospiotr@tlen.pl>
2018-05-18 08:43:13 +0200

    Update startup-bootstrap.sh

--- startup-bootstrap.sh
+++ startup-bootstrap.sh
@@ -6,8 +6,7 @@ echo "Current branch is: ${BRANCH}"
 
 bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}/task-consul-install.sh?nocache") > /var/log/task-consul-install.log
 bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}/task-nomad-install.sh?nocache") > /var/log/task-nomad-install.log
-bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}/task-linkerd-install.sh?nocache") > /var/log/task-linkerd-install.log
+bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}/task-java-install.sh?nocache") > /var/log/task-linkerd-install.log
 
 bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}/task-consul-run-bootstrap.sh?nocache") > /var/log/task-consul-run.log
 bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}/task-nomad-run.sh?nocache") > /var/log/task-nomad-run.log
-bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}/task-linkerd-run.sh?nocache") > /var/log/task-linkerd-run.log
