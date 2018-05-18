2355c3663646480f5dd0fd88ae7a853b2d2a7590
Piotr Kosmowski <kospiotr@tlen.pl>
2018-05-18 08:50:16 +0200

    Update startup-instance.sh

--- startup-instance.sh
+++ startup-instance.sh
@@ -7,7 +7,7 @@ echo "Current branch is: ${BRANCH}"
 echo "Installing dependencies - will be Ansible/Chef/Pupet responsibility for box configuration"
 bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}/task-consul-install.sh?nocache") > /var/log/task-consul-install.log
 bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}/task-nomad-install.sh?nocache") > /var/log/task-nomad-install.log
-bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}/task-java-install.sh?nocache") > /var/log/task-linkerd-install.log
+bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}/task-java-install.sh?nocache") > /var/log/task-java-install.log
 
 echo "Running services - will be startup scripts responsibility"
 bash <(wget -qO- "https://raw.githubusercontent.com/kospiotr/us-playground-scripts/${BRANCH}/task-consul-run-instance.sh?nocache") > /var/log/task-consul-run.log
