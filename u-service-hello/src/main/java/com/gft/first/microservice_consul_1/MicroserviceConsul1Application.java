1baae5e26084ce1545c1c645885d7ca1f4424712
Unknown <szczeprez@poczta.fm>
2018-05-21 11:39:30 +0200

    Added endpoint /api/inspect

--- u-service-hello/src/main/java/com/gft/first/microservice_consul_1/MicroserviceConsul1Application.java
+++ u-service-hello/src/main/java/com/gft/first/microservice_consul_1/MicroserviceConsul1Application.java
@@ -1,25 +1,38 @@
 package com.gft.first.microservice_consul_1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @Configuration
 @EnableAutoConfiguration
-@EnableDiscoveryClient
 @RestController
 public class MicroserviceConsul1Application {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(com.gft.first.microservice_consul_1.MicroserviceConsul1Application.class);
+
+	@Value("${spring.application.name}")
+	private String name;
+
+	@Autowired
+	private Environment environment;
+
+
 	@RequestMapping("/")
 	public String home() {
-		return "Hello World";
+		return "Hello World MicroserviceConsul1Application";
 	}
 
 	@GetMapping("/my-health-check")
@@ -28,7 +41,32 @@ public class MicroserviceConsul1Application {
 		return new ResponseEntity<>(message, HttpStatus.OK);
 	}
 
+	@GetMapping("/api/inspect")
+	public String apiInspect() {
+
+		InetAddress ip = null;
+		String hostname = null;
+
+		try {
+			ip = InetAddress.getLocalHost();
+			hostname = ip.getHostName();
+
+			LOGGER.info("Application name  : " +  name);
+			LOGGER.info("Your current IP address : " + ip);
+			LOGGER.info("Your current Hostname : " + hostname);
+			LOGGER.info("Your current Local server port : " +  environment.getProperty("local.server.port"));
+
+		} catch (UnknownHostException e) {
+			LOGGER.error("Lollger UnknownHostException" + e);
+		}
+
+		return "Application name  : <b>" +  name +
+				"</b><br/>Your current IP address : <b>" + ip +
+				"</b><br/> Your current Hostname : <b>" + hostname +
+				"</b><br/>Your current Local server port  : <b>" +  environment.getProperty("local.server.port") + "</b>";
+	}
+
 	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceConsul1Application.class, args);
+		SpringApplication.run(com.gft.first.microservice_consul_1.MicroserviceConsul1Application.class, args);
 	}
 }
